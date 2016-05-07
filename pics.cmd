5d17b2cbf92a1130dbb0b697e21575344f447bf7
Erik Bachmann <Erik@Clicketyclick.dk>
2016-05-07 09:16:36 +0200

    Enhanced list of raw images + Title bar

--- pics.cmd
+++ pics.cmd
@@ -19,7 +19,8 @@ SET STUB=%~dp0
 ::SET VERSION=01.230&SET REVISION=2014-08-07T12:42&SET COMMENT=Eject Card after copying
 ::SET VERSION=01.240&SET REVISION=2014-09-14T09:55&SET COMMENT=title and msg
 ::SET VERSION=01.250&SET REVISION=2014-12-21T10:02&SET COMMENT=ShowTime with leading digits
-  SET VERSION=01.252&SET REVISION=2014-12-21T10:03&SET COMMENT=Target dir test2 to new
+::SET VERSION=01.252&SET REVISION=2014-12-21T10:03&SET COMMENT=Target dir test2 to new
+  SET VERSION=01.253&SET REVISION=2016-05-07T09:03&SET COMMENT=Enhanced list of raw images + Title bar
 ::
 :: TO DO
 :: Use temp files for testing and finding images to copy!
@@ -31,9 +32,40 @@ SET STUB=%~dp0
 ::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers\Handlers\TweakUIAutoplay_109771953
 :: Match arguments in:
 :: HKEY_CLASSES_ROOT\TweakUIAutoplay\shell\TweakUIAutoplay_109771953\command
-
+::
+:: URL: https://en.wikipedia.org/wiki/Raw_image_format
+:: Raw filename extensions and respective camera manufacturers
+:: 
+::     .3fr (Hasselblad)
+::     .ari (ARRIFLEX)
+::     .arw .srf .sr2 (Sony)
+::     .bay (Casio)
+::     .crw .cr2 (Canon)
+::     .cap .iiq .eip (Phase_One)
+::     .dcs .dcr .drf .k25 .kdc (Kodak)
+::     .dng (Adobe)
+::     .erf (Epson)
+::     .fff (Imacon/Hasselblad raw)
+::     .mef (Mamiya)
+::     .mdc (Minolta, Agfa)
+::     .mos (Leaf)
+::     .mrw (Minolta, Konica Minolta)
+::     .nef .nrw (Nikon)
+::     .orf (Olympus)
+::     .pef .ptx (Pentax)
+::     .pxn (Logitech)
+::     .R3D (RED Digital Cinema)
+::     .raf (Fuji)
+::     .raw .rw2 (Panasonic)
+::     .raw .rwl .dng (Leica)
+::     .rwz (Rawzor)
+::     .srw (Samsung)
+::     .x3f (Sigma)
+:: 
+::*********************************************************************
 ECHO %NAME% v. %VERSION% -- %DESCRIPTION%
-TITLE %NAME% v. %VERSION% Rev. %Revision%
+SET _TitleStub=%NAME% v. %VERSION% Rev. %Revision%
+TITLE %_TitleStub% 
 ECHO Rev. %Revision%
 ECHO By %Author%
     ECHO:
@@ -41,11 +73,10 @@ ECHO By %Author%
     CALL :Init %*
     ECHO Counting files
     CALL :Count_files
-::ECHO ON
     CALL :Show_filecount
     CALL :main
     :: CALL :TEST
-	::CALL :EjectCard
+    ::CALL :EjectCard
     CALL :show_Timer
     PAUSE
 GOTO :EOF
@@ -57,10 +88,11 @@ GOTO :EOF
     CD /D "%_Root%"
     SET _ERRORS=0
     SET _FILES=0
-    SET _Total_FILES=0
+    ::SET _Total_FILES=0
     SET _Total_files_=0
     SET STANDARD_EXTENTIONS=jpg
-    SET RAW_EXTENTIONS=dng
+    ::SET RAW_EXTENTIONS=dng pef tif 
+    SET RAW_EXTENTIONS=dng pef tif crw cr2 nef nrw orf
     SET MOVIE_EXTENTIONS=avi mov thm
     SET _SD-DRIVE=E:
 
@@ -81,7 +113,7 @@ GOTO :EOF   *** :init ***
     FOR %%j IN (%STANDARD_EXTENTIONS%) DO (
         FOR /F %%i IN ('DIR /B /S %_SD-DRIVE%\*.%%j 2^>NUL') DO (
             CALL :Copy_pic %%~nxi %%~ti %%~dpi new\
-			REM ::CALL :Copy_pic %%~nxi %%~ti %%~dpi test2\
+            REM ::CALL :Copy_pic %%~nxi %%~ti %%~dpi test2\
         )
     )
     ECHO:
@@ -144,7 +176,7 @@ GOTO :EOF   *** :SetEuroDate ***
     )
 
     IF NOT EXIST %5%_dato:~0,10%\%_dato%_%1 (
-		TITLE %NAME%: Copying %_dato%_%1
+        TITLE %NAME%: Copying %_dato%_%1
         COPY %4%1 %5%_dato:~0,10%\%_dato%_%1 >>%_root%.error.txt 2>&1
         IF ERRORLEVEL 1 ECHO FEJL & SET /A _ERRORS=%_ERRORS% + 1 & pause
         ::CALL SET /A _FILES_%_FILE:~-3%=%_FILES_%_FILE:~-3%% + 1
@@ -152,9 +184,11 @@ GOTO :EOF   *** :SetEuroDate ***
         CALL :add_one _FILES_%_FILE:~-3%
         SET /p _=o<nul
     ) ELSE (
-	    rem TITLE %$NAME%: Skipping %_dato%_%1
+        rem TITLE %$NAME%: Skipping %_dato%_%1
+        CALL SET /A _FILEs=%_FILES% + 1
         SET /p _=.<nul
     )
+    TITLE %_TitleStub% - %_FILES% / %_Total_FILES_%
 GOTO :EOF   *** :copy_pic ***
 
 ::----------------------------------------------------------------------
@@ -191,8 +225,8 @@ GOTO :EOF   *** :show ***
 :EjectCard
     ECHO %0
     %SystemRoot%\System32\rundll32.exe shell32.dll,Control_RunDLL hotplug.dll
-	ECHO %0 DONE
-GOTO :EOF	*** :EjectCard ***
+    ECHO %0 DONE
+GOTO :EOF    *** :EjectCard ***
 
 ::----------------------------------------------------------------------
 
@@ -230,46 +264,46 @@ GOTO :EOF   *** :set_sd ***
 
 :start_Timer
  
-	FOR /F "skip=1 tokens=1-6" %%A IN ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Second /Format:table ^| findstr /r "."') DO (
-		set Milisecond=%time:~9,2% 
-		set Day=%%A
-		set Hour=%%B
-		set Minute=%%C
-		set Second=%%D
-	)
-	set /a Start=%Day%*8640000+%Hour%*360000+%Minute%*6000+%Second%*100+%Milisecond%
+    FOR /F "skip=1 tokens=1-6" %%A IN ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Second /Format:table ^| findstr /r "."') DO (
+        SET Milisecond=%time:~9,2% 
+        SET Day=%%A
+        SET Hour=%%B
+        SET Minute=%%C
+        SET Second=%%D
+    )
+    SET /a Start=%Day%*8640000+%Hour%*360000+%Minute%*6000+%Second%*100+%Milisecond%
 
 GOTO :EOF
 
 :show_timer
-	FOR /F "skip=1 tokens=1-6" %%A IN ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Second /Format:table ^| findstr /r "."') DO (
-		set Day=%%A
-		set Hour=%%B
-		set Minute=%%C
-		set Second=%%D
-	)
-	set Milisecond=%time:~9,2% 
-	set /a End=%Day%*8640000+%Hour%*360000+%Minute%*6000+%Second%*100+%Milisecond%
-	set /a Diff=%End%-%Start%
-	set /a DiffMS=%Diff%%%100
-	set /a Diff=(%Diff%-%DiffMS%)/100
-	set /a DiffSec=%Diff%%%60
-	set /a Diff=(%Diff%-%Diff%%%60)/60
-	set /a DiffMin=%Diff%%%60
-	set /a Diff=(%Diff%-%Diff%%%60)/60
-	set /a DiffHrs=%Diff%
-	 
-	:: format with leading zeroes
-::	if %DiffMS% LSS 10 set DiffMS=0%DiffMS!%
-::	if %DiffSec% LSS 10 set DiffMS=0%DiffSec%
-::	if %DiffMin% LSS 10 set DiffMS=0%DiffMin%
-::	if %DiffHrs% LSS 10 set DiffMS=0%DiffHrs%
-	set DiffMS=00%DiffMS%
-	set DiffSec=00%DiffSec%
-	set DiffMin=00%DiffMin%
-	set DiffHrs=00%DiffHrs%
-	 
-	ECHO Duration: %DiffHrs:~-2%:%DiffMin:~-2%:%DiffSec:~-2%.%DiffMS:~-2%
+    FOR /F "skip=1 tokens=1-6" %%A IN ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Second /Format:table ^| findstr /r "."') DO (
+        SET Day=%%A
+        SET Hour=%%B
+        SET Minute=%%C
+        SET Second=%%D
+    )
+    SET Milisecond=%time:~9,2% 
+    SET /a End=%Day%*8640000+%Hour%*360000+%Minute%*6000+%Second%*100+%Milisecond%
+    SET /a Diff=%End%-%Start%
+    SET /a DiffMS=%Diff%%%100
+    SET /a Diff=(%Diff%-%DiffMS%)/100
+    SET /a DiffSec=%Diff%%%60
+    SET /a Diff=(%Diff%-%Diff%%%60)/60
+    SET /a DiffMin=%Diff%%%60
+    SET /a Diff=(%Diff%-%Diff%%%60)/60
+    SET /a DiffHrs=%Diff%
+     
+    :: format with leading zeroes
+::    if %DiffMS% LSS 10 SET DiffMS=0%DiffMS!%
+::    if %DiffSec% LSS 10 SET DiffMS=0%DiffSec%
+::    if %DiffMin% LSS 10 SET DiffMS=0%DiffMin%
+::    if %DiffHrs% LSS 10 SET DiffMS=0%DiffHrs%
+    SET DiffMS=00%DiffMS%
+    SET DiffSec=00%DiffSec%
+    SET DiffMin=00%DiffMin%
+    SET DiffHrs=00%DiffHrs%
+    ECHO: 
+    ECHO Duration: %DiffHrs:~-2%:%DiffMin:~-2%:%DiffSec:~-2%.%DiffMS:~-2%
 GOTO :EOF
 
 ::*** End of File ******************************************************
